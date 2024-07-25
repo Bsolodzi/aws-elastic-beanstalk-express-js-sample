89a3e7105be4c2ab00055e564e2b52f849b6c64c
Bernard Kwame Solozi <bsolodzi@gmail.com>
2024-07-25 12:13:36 +0000

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hi, this is Bernard Kwame Solodzi'));
+app.get('/', (req, res) => res.send('Hi, this is Bernard Kwame Solodzi and this is the final pipeline test'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
